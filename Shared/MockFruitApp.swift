cd37b7f7dada31bb625722b921e8a4378878f9a6
Leo Dion <leo@Leos-Monterey-VM-Mac.local>
2024-02-15 09:41:24 -0500

    project start

---
+++ Shared/MockFruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockFruitApp.swift
+//  Shared
+//
+//  Created by Leo Dion on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MockFruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
